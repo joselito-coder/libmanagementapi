4ace04fc27157088c7e56810b03a233480b5167b
joselito <joselitorebello20@gmail.com>
2022-12-12 21:47:35 +0530

    Implemeted Security using helmet js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // Configure Environment variables
 require('dotenv').config();
+// imports
 const express = require("express");
 const app = express();
 const books = require("./routes/books.route");
 const authors = require('./routes/authors.route');
 const auth = require('./routes/auth.route');
 const connectDb = require('./db/connect');
+const helmet = require('helmet');
 
 //  port to listen to
 const port = process.env.PORT || 3000;
@@ -13,6 +15,8 @@ const port = process.env.PORT || 3000;
 
 // Middlewares
 app.use(express.json());
+// use helmet js to set response headers
+app.use(helmet())
 
 
 // routes
@@ -22,25 +26,25 @@ app.use('/api/v1/auth', auth);
 
 
 app.get("/", (req, res) => {
-  res.send("Please use /api/v1/books for books\n And /api/v1/author for authors \n Use /api/v1/auth for Authentication");
+    res.send("Please use /api/v1/books for books\n And /api/v1/author for authors \n Use /api/v1/auth for Authentication");
 });
 
 
 
 // Start the app only if databse is connected
-const start = async () => {
-  try {
+const start = async() => {
+    try {
 
-    await connectDb(process.env.MONGO_URI);
-    app.listen(port, () => {
-      console.log(`the app has started on port ${port}`);
-    });
+        await connectDb(process.env.MONGO_URI);
+        app.listen(port, () => {
+            console.log(`the app has started on port ${port}`);
+        });
 
-  } catch (error) {
+    } catch (error) {
 
-    console.error(error);
+        console.error(error);
 
-  }
+    }
 }
 
 start();
